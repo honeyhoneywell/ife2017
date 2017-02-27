357ac27828fe25bcd8d2e9454b9b3ed1a4474011
xsheng1992 <a415441091@hotmail.com>
2017-02-27 11:37:23 +0800

    regular-1 add

---
+++ regular-1/js/regular.js
@@ -0,0 +1,30 @@
+//验证手机号码
+function isPhoneNumber() {
+	var numstring = document.querySelector('#pNum').value,
+		regexp = /^1[35874]\d{9}$/;
+
+	if(regexp.test(numstring)) {
+		alert("正确的电话号码！");
+	} else {
+		alert("非法的电话号码！");
+	}
+}
+
+//验证相邻重复单词
+function isSameWord() {
+	var string = document.querySelector('#pStr').value,
+		regexp = /\b(\w+)\b\s+\1/;
+
+	if(regexp.test(string)) {
+		alert("有相邻重复单词！");
+	} else {
+		alert("没有相邻重复单词！");
+	}
+}
+
+//获取按钮
+var numBtn = document.querySelector("#numBtn");
+var strBtn = document.querySelector("#strBtn");
+//绑定事件
+numBtn.addEventListener('click', isPhoneNumber);
+strBtn.addEventListener('click', isSameWord);
